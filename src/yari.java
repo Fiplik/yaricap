814f1cf268bf39d677678f77e642472c537f5386
Fiplik <101054670+Fiplik@users.noreply.github.com>
2022-03-06 22:34:37 +0300

    s

---
+++ src/yari.java
@@ -0,0 +1,29 @@
+import  java.util.Scanner;
+public class yari {
+    public static void main(String[] args) {
+        int r;
+        double cevre,alan,pi=3.14;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Yari Capini Giriniz: ");
+        r =input.nextInt();
+                alan=(pi*r*r);
+                cevre=(2*pi*r);
+System.out.println("************************************");
+        System.out.println("Dairenin Alanı: "+alan);
+        System.out.println("Dairenin Cevresi: "+cevre);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
